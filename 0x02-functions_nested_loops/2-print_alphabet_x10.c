8e12bb333eb8594e136e649332555ec5808ebe72
Ebenezar92 <gebenezar@gmail.com>
2022-09-15 01:07:57 -0700

    my task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**print_alphabet_x10 - function to print abc 10 times
+ *
+ * return 0
+ */
+void print_alphabet_x10(void)
+{
+	char ebenezar, i;
+
+	for (i = 0 ; i < = 9; i++)
+       	{
+		for (ebenezar = 'a'; ebenezar <= 'z'; ebenezar++)
+		{
+			_putchar(ebenezar);
+		}
+		_putchar('\n');
+	}
+}

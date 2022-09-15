59f199b727372872f9d1cd64ce4b7c5ee756ea69
Ebenezar92 <gebenezar@gmail.com>
2022-09-15 00:43:17 -0700

    My task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print alphabet - function to print abc
+ *
+ * return 0
+ */
+void print_alphabet(void)
+{
+	char Ebenezar;
+
+	for (Ebenezar = 'a'; Ebenezar <= 'z' Ebenezar++)
+	{
+		_putchar(Ebenezar);
+	}
+_putchar('\n')
+}
